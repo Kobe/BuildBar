19250814b00c0b32f468935a226ff0fb9ada6c24
Kobe <81110+Kobe@users.noreply.github.com>
2025-07-26 23:30:09 +0200

    Initial Commit

---
+++ BuildBar/BuildBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildBarApp.swift
+//  BuildBar
+//
+//  Created by Ingo Stöcker on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct BuildBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
